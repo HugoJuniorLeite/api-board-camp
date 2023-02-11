7b8a757052c9f042fe9fd007cbe569ffa87e013e
Hugo Junior <hjunioreng@gmail.com>
2023-02-11 07:53:14 -0300

    fix: conect server

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -11,3 +11,16 @@ try {
     res.status(500).send(error.message)
 }
 }
+
+export async function insertGame(req,res){
+const {name, image, stockTotal,pricePerDay} = req.body
+
+    try {
+        const newGame = await db.query(`INSERT INFO games( 
+            name, image, stockTotal,pricePerDay) VALUES($1,$2,$3,$4);`,[name, image, stockTotal,pricePerDay])
+            res.send(201)
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
