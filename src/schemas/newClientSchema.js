b8842d00c499de5bdca612ee4cc9e235ddc1f240
Hugo Junior <hjunioreng@gmail.com>
2023-02-12 09:57:15 -0300

    fix: rota post client

--- src/schemas/newClientSchema.js
+++ src/schemas/newClientSchema.js
@@ -4,6 +4,6 @@ export const newClientSchema =Joi.object({
 name: Joi.string().required(),
 phone: Joi.number().min(10).max(11).required,
 cpf: Joi.number().min(11).max(11).required(),
-birthday: Joi.number().required(),
+birthday: Joi.string().required(),
 })
 
