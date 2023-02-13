9df0e05e3e2e5810aa2af0fb7db6104776876e48
Hugo Junior <hjunioreng@gmail.com>
2023-02-12 22:46:00 -0300

    FIX: rentals

--- src/middlewares/newRentalsValidation.js
+++ src/middlewares/newRentalsValidation.js
@@ -1,2 +1,26 @@
+import {db} from "../database/database.connection.js"
+
+export async function newRentalsValidation(req, res, next){
+
+const {gameId} = req.body
+
+
+try {
+    const gameAvailable = await db.query(`SELECT * FROM games WHERE id = $1`,[gameId]);
+
+ //   console.log(existName.rows.length > 0 )
+
+   if(gameAvailable.rows[0].stockTotal < 1){
+
+    return res.sendStatus(400)
+   }
+
+    next()
+
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+
 
 
--- src/routers/rentals.routers.js
+++ src/routers/rentals.routers.js
@@ -2,10 +2,16 @@ import { Router } from "express";
 
 import { insertRentals, listRentals } from "../controllers/rentals.controler.js";
 
+
+
+import { validateSchema } from "../middlewares/validadeSchema.js";
+import { newRentalSchema } from "../schemas/newRentalSchema.js";
+
+
 const rentalsRouter = Router()
 
 
 rentalsRouter.get("/rentals", listRentals);
-rentalsRouter.post("/rentals",insertRentals);
+rentalsRouter.post("/rentals",validateSchema(newRentalSchema), insertRentals);
 
 export default rentalsRouter;
